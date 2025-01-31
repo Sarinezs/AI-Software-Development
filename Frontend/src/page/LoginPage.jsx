ec132fc4fdf2feb06ebef48e9f4b9c1f36b9bc10
Sarinez <a.edr.edm.ed@gmail.com>
2025-01-31 23:25:34 +0700

    making login page

--- Frontend/src/page/LoginPage.jsx
+++ Frontend/src/page/LoginPage.jsx
@@ -2,89 +2,109 @@ import { useState, useEffect } from 'react'
 import { GoogleLogin, GoogleLogout } from 'react-google-login'
 import { gapi } from 'gapi-script'
 import axios from 'axios'
+import './LoginPage.css'
 
 const LoginPage = () => {
 
     const clientId = "11247620455-hdn7a4bois1qleisef5aaobjaq9ijd2v.apps.googleusercontent.com"
-	const [profile, setProfile] = useState(null)
-	const [userdata, setUserdata] = useState({
-		username: '',
-		email: '',
-		password: '',
-	})
+    const [profile, setProfile] = useState(null)
+    const [userdata, setUserdata] = useState({
+        username: '',
+        email: '',
+        password: '',
+    })
 
-	useEffect(() => {
-		const initClient = () => {
-			gapi.client.init({
-				clientId: clientId,
-				scope: ''
-			})
-		}
-		gapi.load("client:auth2", initClient)
-	}, [])
+    useEffect(() => {
+        const initClient = () => {
+            gapi.client.init({
+                clientId: clientId,
+                scope: ''
+            })
+        }
+        gapi.load("client:auth2", initClient)
+    }, [])
 
-	const onsuccess = async (res) => {
-		setProfile(res.profileObj)
-		setUserdata(prevstate => ({
-			...prevstate,
-			username: res.profileObj.name,
-			email: res.profileObj.email,
-		}))
-		const payload = {
-			username: res.profileObj.name,
-			email: res.profileObj.email,
-			password: userdata.password, // ใช้ password จาก state
-		};
-		try {
-			const list = await axios.post("http://localhost:8000/register", payload);
-			console.log('success', list);
-		} catch (error) {
-			console.error('Error during registration:', error);
-		}
-	}
+    const onsuccess = async (res) => {
+        setProfile(res.profileObj)
+        setUserdata(prevstate => ({
+            ...prevstate,
+            username: res.profileObj.name,
+            email: res.profileObj.email,
+        }))
+        const payload = {
+            username: res.profileObj.name,
+            email: res.profileObj.email,
+            password: userdata.password, // ใช้ password จาก state
+        };
+        try {
+            const list = await axios.post("http://localhost:8000/register", payload);
+            console.log('success', list);
+        } catch (error) {
+            console.error('Error during registration:', error);
+        }
+    }
 
-	const onfailure = (res) => {
-		console.log('failed', res)
-	}
+    const onfailure = (res) => {
+        console.log('failed', res)
+    }
 
-	const fetchUsers = async () => {
-		try {
-			const authtoken = localStorage.getItem('token')
-			const response = await axios.get('http://localhost:8000/user', {
-				headers: {
-					'authorization': `Bearer ${authtoken}`
-				}
-			});
-			console.log(response.data.users[4].email); // อัปเดต state users ด้วยข้อมูลที่ได้จาก API
-		} catch (err) {
-			console.error('Error fetching users:', err);
-		}
-	};
+    const fetchUsers = async () => {
+        try {
+            const authtoken = localStorage.getItem('token')
+            const response = await axios.get('http://localhost:8000/user', {
+                headers: {
+                    'authorization': `Bearer ${authtoken}`
+                }
+            });
+            console.log(response.data.users[4].email); // อัปเดต state users ด้วยข้อมูลที่ได้จาก API
+        } catch (err) {
+            console.error('Error fetching users:', err);
+        }
+    };
 
-	const logIn = async () => {
-		try {
-			
-			const list = await axios.post("http://localhost:8000/login", {
-				email: "arm50@mail.com",
+    const logIn = async () => {
+        try {
+
+            const list = await axios.post("http://localhost:8000/login", {
+                email: "arm50@mail.com",
                 password: "12345",
-			});
-			localStorage.setItem('token', list.data.token)
-			console.log('success', list);
-		} catch (error) {
-			console.error('Error during registration:', error);
-		}
-	}
+            });
+            localStorage.setItem('token', list.data.token)
+            console.log('success', list);
+        } catch (error) {
+            console.error('Error during registration:', error);
+        }
+    }
 
-	const logOut = () => {
-		setProfile(null)
-	}
+    const logOut = () => {
+        setProfile(null)
+    }
 
 
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div className='signin-main'>
+            <div className='signin-box'>
+                <h1 className='sign-title'>Sign In</h1>
+                <input className='signin-input' type="text" placeholder='Email' />
+                <input className='signin-input' type="password" placeholder='Password' />
+                <button className='signin-btn'>Sign In</button>
+                <p className='signin-separate1'>Don't have an account? <a href="">Sign Up</a></p>
+                <div className='signin-separate2'>
+                    <hr/>
+                    <p>or</p>
+                </div>
+                <div className='signin-google'>
+                    <GoogleLogin
+                        clientId={clientId}
+                        buttonText="Sign In With Google"
+                        onSuccess={onsuccess}
+                        onFailure={onfailure}
+                        cookiePolicy={'single_host_origin'}
+                    />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default LoginPage
